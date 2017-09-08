f5d6ae1e103cce7f9f8840587ac14d6b6d5ae15d
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2017-09-08 15:25:11 +0200

    Add project files.

---
+++ Application.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace GreatCodNet
+{
+    using SFML.Graphics;
+
+    public class Application
+    {
+        private RenderWindow RenderWindow;
+
+        public Application()
+        {
+            RenderWindow = new RenderWindow(new SFML.Window.VideoMode(1024, 768), "Application");
+            RenderWindow.MouseButtonPressed += RenderWindow_MouseButtonPressed;
+            RenderWindow.KeyPressed += RenderWindow_KeyPressed;
+            RenderWindow.Closed += (sender, e) => RenderWindow.Close();
+        }
+
+        public void Run()
+        {
+            while (RenderWindow.IsOpen)
+            {
+                Render();
+                RenderWindow.WaitAndDispatchEvents();
+            }            
+        }
+
+        private void Render()
+        {
+            RenderWindow.Clear();
+
+            RenderWindow.Display();
+        }
+
+        private void RenderWindow_KeyPressed(object sender, SFML.Window.KeyEventArgs e)
+        {
+            System.Console.WriteLine("Keypress");
+        }
+
+        private void RenderWindow_MouseButtonPressed(object sender, SFML.Window.MouseButtonEventArgs e)
+        {
+            System.Console.WriteLine("Mouseclick");
+        }
+    }
+}
